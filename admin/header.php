f6cdeb28c015721ee3fb458f2d588ae68d5391b6
John Mark <johnmark.cuyos@evsu.edu.ph>
2024-08-13 18:09:05 +0800

    fix

--- admin/header.php
+++ admin/header.php
@@ -1,12 +1,11 @@
 <!-- jquery -->
-<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-
+<script src="plugins/jquery/jquery.min.js"></script>
 
 <!-- bootstrap -->
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
 <!-- Font Awesome -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
 
 <!-- adminlte template -->
 <script src="dist/js/adminlte.min.js"></script>
@@ -16,9 +15,8 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
 <!-- datatables -->
-<link rel="stylesheet" href="https://cdn.datatables.net/2.1.3/css/dataTables.dataTables.min.css">
-<script src="https://cdn.datatables.net/2.1.3/js/dataTables.min.js"></script>
-
+<link rel="stylesheet" href="https://cdn.datatables.net/2.1.3/css/dataTables.dataTables.css" />
+<script src="https://cdn.datatables.net/2.1.3/js/dataTables.js"></script>
 
 <!-- custom css -->
 <link rel="stylesheet" href="css/style.css">
