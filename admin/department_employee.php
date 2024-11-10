61ab51e14ff48aeae7d8955b41e5ff71c6381e34
Kael <johnmark.cuyos@evsu.edu.ph>
2024-11-10 17:38:06 +0800

    fix department directory

--- admin/department_employee.php
+++ admin/department_employee.php
@@ -48,19 +48,17 @@ ORDER BY s.id ASC");
                                 <tr>
                                     <th class="text-center">#</th>
                                     <th class="text-center w-25">School ID</th>
-                                    <th class="text-center w-50">Name</th>
-                                    <th class="text-center w-25">Contact No.</th>
+                                    <th class="text-center w-75">Name</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php
                                 while ($row = $cats->fetch_assoc()):
                                 ?>
-                                    <tr onclick="window.location.href='index.php?page=student_view&uid=<?= $row['id'] ?>'">
+                                    <tr onclick="window.location.href='index.php?page=employee_view&uid=<?= $row['id'] ?>'">
                                         <td class="text-center"><?= $i++; ?></td>
                                         <td class="text-center"><?php echo $row['school_id']; ?></td>
                                         <td class="text-left"><?php echo $row['fname'] . ' ' . $row['lname']; ?></td>
-                                        <td class="text-left"><?php echo $row['cellnum']; ?></td>
                                     </tr>
 
                                 <?php endwhile; ?>
