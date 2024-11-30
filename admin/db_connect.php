8816f93aef1714a1e49a2ceb70c07a2ad7fc9d14
Kael <johnmark.cuyos@evsu.edu.ph>
2024-11-30 15:00:25 +0800

    try

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,5 +1,4 @@
-<?php 
+<?php
 
-$conn= new mysqli('localhost','root','','rfidsystem_db')or die("Could not connect to mysql".mysqli_error($conn));
+$conn = new mysqli('localhost', 'u260856940_root', 'rK9HlcUjK=', 'u260856940_rfidsystem_db') or die("Could not connect to mysql" . mysqli_error($conn));
 $conn->query("SET time_zone = '+08:00'");
-?>
---
+++ credentials.php
@@ -0,0 +1,3 @@
+<?php
+$mailUsername = "000phpmailer@gmail.com";
+$mailPassword = "kpii imxs inna jmqo";
